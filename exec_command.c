28ee4f4409765604fd13b9f56d703dc33d1c614c
SafaFarah <kangjisook13@gmail.com>
2024-07-14 04:33:31 +0300

    update main.h

--- exec_command.c
+++ exec_command.c
@@ -1,42 +1,49 @@
 #include "main.h"
 
 /**
- * execve_cmd - function that executes a command
- * @argv: NULL terminated array of strings
- *
- * Return: status of the executed command
- */
+* execve_cmd - function to execute the command.
+* @argv: array of strings holds commands
+* Return: return 0 in success, -1
+*/
 int execve_cmd(char **argv)
 {
-    pid_t pid;
-    int status;
+	char *command = NULL;
+	char *real_command = NULL;
+	int pid;
+	int i;
 
-    pid = fork();
-    if (pid == -1)
-    {
-        perror("Error:");
-        return (127);  /* Common exit code for command not found*/
-    }
-    if (pid == 0)
-    {
-        if (execve(argv[0], argv, NULL) == -1)
-        {
-            perror(argv[0]);
-            exit(2);  /* Exit with status 2 if execve fails*/
-        }
-    }
-    else
-    {
-        wait(&status);
-        if (WIFEXITED(status))
-        {
-            return (WEXITSTATUS(status));
-        }
-        else if (WIFSIGNALED(status))
-        {
-            return (128 + WTERMSIG(status));  /*Return signal number plus 128*/
-        }
-    }
-    return (0);
+	if (argv)
+	{
+		command = argv[0];
+		real_command = get_path(command);
+		if (real_command == NULL)
+		{
+			perror("Error:");
+			free(real_command);
+			for (i = 0; argv[i]; i++)
+				free(argv[i]);
+			return (0);
+		}
+		if (real_command != NULL)
+		{
+			pid = fork();
+			if (pid == 0)
+			{
+				if (execve(real_command, argv, NULL) == -1)
+				{
+					for (i = 1; argv[i] != NULL; i++)
+						free(argv[i]);
+					free(real_command);
+					perror("Error");
+					exit(EXIT_FAILURE);
+				}
+			}
+			else
+			{
+				wait(NULL);
+			}
+			free(real_command);
+		}
+	}
+	return (1);
 }
-
